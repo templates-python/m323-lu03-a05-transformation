f1ec9f3d35c37ac7d928ed8bf4ce4fa07dbebfb9
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-01 12:50:04 +0200

    amend test

--- main_test.py
+++ main_test.py
@@ -11,6 +11,17 @@ def test_increase_salary_by_department():
     assert updated_employees[0]['salary'] == 5000, f"Expected 5000, got {updated_employees[0]['salary']}"
 
 
+def test_filter_by_age():
+    employees = [
+        {'name': 'Alice', 'age': 30, 'salary': 5000, 'department': 'HR'},
+        {'name': 'Bob', 'age': 40, 'salary': 6000, 'department': 'IT'}
+    ]
+    older_employees = main.filter_by_age(employees, 35, 'greater')
+
+    assert len(older_employees) == 1, f"Expected 1, got {len(older_employees)}"
+    assert older_employees[0]['name'] == 'Bob', f"Expected 'Bob', got {older_employees[0]['name']}"
+
+
 def test_convert_names_to_uppercase():
     employees = [
         {'name': 'Alice', 'age': 30, 'salary': 5000, 'department': 'HR'},
@@ -20,20 +31,6 @@ def test_convert_names_to_uppercase():
 
     assert updated_employees[0]['name'] == 'ALICE', f"Expected 'ALICE', got {updated_employees[0]['name']}"
     assert updated_employees[1]['name'] == 'BOB', f"Expected 'BOB', got {updated_employees[1]['name']}"
-
-
-def test_transform_employee_data():
-    employees = [
-        {'name': 'Alice', 'age': 30, 'salary': 5000, 'department': 'HR'},
-        {'name': 'Bob', 'age': 40, 'salary': 6000, 'department': 'IT'}
-    ]
-
-    def dummy_transformation(employees):
-        return employees
-
-    transformed_employees = main.transform_employee_data(employees, dummy_transformation)
-
-    assert transformed_employees == employees, f"Expected {employees}, got {transformed_employees}"
 
 
 def test_transform_employee_data():
